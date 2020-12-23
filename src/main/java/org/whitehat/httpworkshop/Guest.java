7ac8c8ea567ef7d8a0eacfdaea377f711b7f0770
Bernard Mordan <bernard@tableflip.io>
2020-12-23 18:27:05 +0000

    Update Guest.java

--- src/main/java/org/whitehat/httpworkshop/Guest.java
+++ src/main/java/org/whitehat/httpworkshop/Guest.java
@@ -1,13 +1,13 @@
 package org.whitehat.httpworkshop;
 
 public class Guest {
-    private String guests;
+    private String[] guests;
 
     public void setGuests(String guests) {
-        this.guests = guests;
+        this.guests = guests.split(",\s*");
     }
 
-    public String getGuests() {
+    public String[] getGuests() {
         return this.guests;
     }
 }
